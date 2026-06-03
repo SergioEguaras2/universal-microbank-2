5e5f0f88719a8bdeeedda9750b4748fd163b5e1d
cesarherrero <cesar.herrero@seidor.com>
2026-06-03 20:19:08 +0200

    #CHH - test 2

---
+++ blocks/buscador-en-cabecera/buscador-en-cabecera.js
@@ -0,0 +1,8 @@
+/**
+ * Decorador para el bloque buscador-en-cabecera
+ * @param {HTMLElement} block
+ */
+export default function decorate(block) {
+  block.classList.add('buscador-en-cabecera--initialized');
+  // Aquí puedes customizar cómo se estructuran las celdas en el DOM
+}
---
+++ blocks/reproductor-de-v-deo-brightcove/reproductor-de-v-deo-brightcove.js
@@ -0,0 +1,8 @@
+/**
+ * Decorador para el bloque reproductor-de-v-deo-brightcove
+ * @param {HTMLElement} block
+ */
+export default function decorate(block) {
+  block.classList.add('reproductor-de-v-deo-brightcove--initialized');
+  // Aquí puedes customizar cómo se estructuran las celdas en el DOM
+}
